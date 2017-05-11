e68054f6865edef35295e1e76a21eb4a85491e72
Zulus <danil.kazim.99@gmail.com>
2017-05-12 00:26:51 +0300

    set url

--- scripts/install-libprogbase.sh
+++ scripts/install-libprogbase.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase-cpp/archive/master.zip
-unzip libprogbase-cpp.zip -d libprogbase-cpp
-cd libprogbase/libprogbase-cpp-master ; sudo make install
-cd ../../ ; rm libprogbase-cpp/ libprogbase-cpp.zip -rf
 wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase/archive/master.zip
 unzip libprogbase.zip -d libprogbase
 cd libprogbase/libprogbase-master ; sudo make install
 cd ../../ ; rm libprogbase/ libprogbase.zip -rf
+
+wget -O libprogbase.zip https://codeload.github.com/PublicHadyniak/libprogbase-cpp/zip/master
+unzip libprogbase-cpp.zip -d libprogbase-cpp
+cd libprogbase/libprogbase-cpp-master ; sudo make install
+cd ../../ ; rm libprogbase-cpp/ libprogbase-cpp.zip -rf
