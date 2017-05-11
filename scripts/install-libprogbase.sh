c1e143b2420712158cefc82d43cde3aa022120c2
Zulus <danil.kazim.99@gmail.com>
2017-05-12 00:22:28 +0300

    fix .sh

--- scripts/install-libprogbase.sh
+++ scripts/install-libprogbase.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase/archive/master.zip
-unzip libprogbase.zip -d libprogbase
-cd libprogbase/libprogbase-master ; sudo make install
-cd ../../ ; rm libprogbase/ libprogbase.zip -rf
-
 wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase-cpp/archive/master.zip
 unzip libprogbase-cpp.zip -d libprogbase-cpp
 cd libprogbase/libprogbase-cpp-master ; sudo make install
 cd ../../ ; rm libprogbase-cpp/ libprogbase-cpp.zip -rf
+wget -O libprogbase.zip https://github.com/PublicHadyniak/libprogbase/archive/master.zip
+unzip libprogbase.zip -d libprogbase
+cd libprogbase/libprogbase-master ; sudo make install
+cd ../../ ; rm libprogbase/ libprogbase.zip -rf
